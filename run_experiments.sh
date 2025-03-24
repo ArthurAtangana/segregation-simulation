0e9e7061160974e7773732d4180103905b3033f4
ArthurAtangana <arthur.atangana@protonmail.com>
2025-03-24 18:35:08 -0400

    update paths in script

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # run experiments
-./bin/segregation -c experiment_1_config.json -s 500 -o experiment_1_log.csv
-./bin/segregation -c experiment_2_config.json -s 500 -o experiment_2_log.csv
-./bin/segregation -c experiment_3_config.json -s 500 -o experiment_3_log.csv
-./bin/segregation -c experiment_4_config.json -s 500 -o experiment_4_log.csv
-./bin/segregation -c experiment_5_config.json -s 500 -o experiment_5_log.csv
-./bin/segregation -c experiment_6_config.json -s 500 -o experiment_6_log.csv
+./bin/segregation -c config/experiment_1_config.json -s 500 -o logs/experiment_1_log.csv
+./bin/segregation -c config/experiment_2_config.json -s 500 -o logs/experiment_2_log.csv
+./bin/segregation -c config/experiment_3_config.json -s 500 -o logs/experiment_3_log.csv
+./bin/segregation -c config/experiment_4_config.json -s 500 -o logs/experiment_4_log.csv
+./bin/segregation -c config/experiment_5_config.json -s 500 -o logs/experiment_5_log.csv
+./bin/segregation -c config/experiment_6_config.json -s 500 -o logs/experiment_6_log.csv
