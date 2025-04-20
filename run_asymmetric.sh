8f0856e57de91a87ec85fc7c221afbc6cf3d4f81
ArthurAtangana <arthur.atangana@protonmail.com>
2025-04-20 19:31:50 -0400

    separate run scripts into sym and asym

---
+++ run_asymmetric.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# build asymmetric-cell-devs simulation
+./asymmetric-cell-devs/build_sim.sh
+# run experiments
+./cell-devs/bin/segregation -c config/asymmetric-cell-devs/experiment_1_config.json -t 500 -o logs/asymmetric-cell-devs/experiment_1_log.csv
+# ./cell-devs/bin/segregation -c config/asymmetric-cell-devs/experiment_2_config.json -t 500 -o logs/asymmetric-cell-devs/experiment_2_log.csv
+# ./cell-devs/bin/segregation -c config/asymmetric-cell-devs/experiment_3_config.json -t 500 -o logs/asymmetric-cell-devs/experiment_3_log.csv
+# ./cell-devs/bin/segregation -c config/asymmetric-cell-devs/experiment_4_config.json -t 500 -o logs/asymmetric-cell-devs/experiment_4_log.csv
+# ./cell-devs/bin/segregation -c config/asymmetric-cell-devs/experiment_5_config.json -t 500 -o logs/asymmetric-cell-devs/experiment_5_log.csv
+# ./cell-devs/bin/segregation -c config/asymmetric-cell-devs/experiment_6_config.json -t 500 -o logs/asymmetric-cell-devs/experiment_6_log.csv
--- run_experiments.sh
+++ run_experiments.sh
@@ -8,13 +8,3 @@
 ./cell-devs/bin/segregation -c config/cell-devs/experiment_4_config.json -t 500 -o logs/cell-devs/experiment_4_log.csv
 ./cell-devs/bin/segregation -c config/cell-devs/experiment_5_config.json -t 500 -o logs/cell-devs/experiment_5_log.csv
 ./cell-devs/bin/segregation -c config/cell-devs/experiment_6_config.json -t 500 -o logs/cell-devs/experiment_6_log.csv
-
-# build asymmetric-cell-devs simulation
-./asymmetric-cell-devs/build_sim.sh
-# run experiments
-./cell-devs/bin/segregation -c config/asymmetric-cell-devs/experiment_1_config.json -t 500 -o logs/asymmetric-cell-devs/experiment_1_log.csv
-./cell-devs/bin/segregation -c config/asymmetric-cell-devs/experiment_2_config.json -t 500 -o logs/asymmetric-cell-devs/experiment_2_log.csv
-./cell-devs/bin/segregation -c config/asymmetric-cell-devs/experiment_3_config.json -t 500 -o logs/asymmetric-cell-devs/experiment_3_log.csv
-./cell-devs/bin/segregation -c config/asymmetric-cell-devs/experiment_4_config.json -t 500 -o logs/asymmetric-cell-devs/experiment_4_log.csv
-./cell-devs/bin/segregation -c config/asymmetric-cell-devs/experiment_5_config.json -t 500 -o logs/asymmetric-cell-devs/experiment_5_log.csv
-./cell-devs/bin/segregation -c config/asymmetric-cell-devs/experiment_6_config.json -t 500 -o logs/asymmetric-cell-devs/experiment_6_log.csv
