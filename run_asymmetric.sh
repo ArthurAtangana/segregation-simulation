1a8789391a32bb2a4d4ef3de1e3290852ab35db7
ArthurAtangana <arthur.atangana@protonmail.com>
2025-04-21 16:52:49 -0400

    update asymmetric run script

--- run_asymmetric.sh
+++ run_asymmetric.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+cd asymmetric-cell-devs
 # build asymmetric-cell-devs simulation
-./asymmetric-cell-devs/build_sim.sh
+./build_sim.sh
 # run experiments
-./asymmetric-cell-devs/bin/segregation -c config/asymmetric-cell-devs/experiment_1_config.json -t 500 -o logs/asymmetric-cell-devs/experiment_1_log.csv
+./bin/segregation -c ../config/asymmetric-cell-devs/default_config.json -t 500 -o ../logs/asymmetric-cell-devs/default_log.csv
+# ./asymmetric-cell-devs/bin/segregation -c config/asymmetric-cell-devs/experiment_1_config.json -t 500 -o logs/asymmetric-cell-devs/experiment_1_log.csv
 # ./cell-devs/bin/segregation -c config/asymmetric-cell-devs/experiment_2_config.json -t 500 -o logs/asymmetric-cell-devs/experiment_2_log.csv
 # ./cell-devs/bin/segregation -c config/asymmetric-cell-devs/experiment_3_config.json -t 500 -o logs/asymmetric-cell-devs/experiment_3_log.csv
 # ./cell-devs/bin/segregation -c config/asymmetric-cell-devs/experiment_4_config.json -t 500 -o logs/asymmetric-cell-devs/experiment_4_log.csv
