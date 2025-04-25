14a65e7ff19f27c4ec63b06c9c41d4390d0a5a57
ArthurAtangana <arthur.atangana@protonmail.com>
2025-04-25 15:45:30 -0400

    Add other experiments and logs

--- run_asymmetric.sh
+++ run_asymmetric.sh
@@ -6,20 +6,33 @@ cd asymmetric-cell-devs
 echo "Building cadmium project"
 echo "--------------------------------------------------"
 ./build_sim.sh
+echo "--------------------------------------------------"
 # run experiments
-echo "--------------------------------------------------"
-echo "Running default experiment"
-echo "--------------------------------------------------"
-./bin/segregation -c ../config/asymmetric-cell-devs/default_config.json -t 500 -o ../logs/asymmetric-cell-devs/default_log.csv
+
+# echo "Running default experiment"
+# echo "--------------------------------------------------"
+# ./bin/segregation -c ../config/asymmetric-cell-devs/default_config.json -t 500 -o ../logs/asymmetric-cell-devs/default_log.csv
 echo "Running experiment 1"
 echo "--------------------------------------------------"
-./bin/segregation -c ../config/asymmetric-cell-devs/experiment_1_config.json -t 100 -o ../logs/asymmetric-cell-devs/experiment_1_log.csv
-echo "Running experiment 2"
+./bin/segregation -c ../config/asymmetric-cell-devs/experiment_1_config.json -t 50 -o ../logs/asymmetric-cell-devs/experiment_1_log.csv
+echo "Running walls experiment 2"
 echo "--------------------------------------------------"
 ./bin/segregation -c ../config/asymmetric-cell-devs/experiment_2_config.json -t 500 -o ../logs/asymmetric-cell-devs/experiment_2_log.csv
-echo "Running experiment 3"
+echo "Running valuable location experiment 3"
 echo "--------------------------------------------------"
 ./bin/segregation -c ../config/asymmetric-cell-devs/experiment_3_config.json -t 500 -o ../logs/asymmetric-cell-devs/experiment_3_log.csv
-echo "Running experiment 4"
+echo "Running valuable location experiment 4"
 echo "--------------------------------------------------"
 ./bin/segregation -c ../config/asymmetric-cell-devs/experiment_4_config.json -t 500 -o ../logs/asymmetric-cell-devs/experiment_4_log.csv
+echo "Running combined experiment 5"
+echo "--------------------------------------------------"
+./bin/segregation -c ../config/asymmetric-cell-devs/experiment_5_config.json -t 500 -o ../logs/asymmetric-cell-devs/experiment_5_log.csv
+echo "Running 50% ratio experiment"
+echo "--------------------------------------------------"
+./bin/segregation -c ../config/asymmetric-cell-devs/ratio_50_config.json  -t 500 -o ../logs/asymmetric-cell-devs/ratio_50_log.csv
+echo "Running 25% ratio experiment"
+echo "--------------------------------------------------"
+./bin/segregation -c ../config/asymmetric-cell-devs/ratio_25_config.json  -t 500 -o ../logs/asymmetric-cell-devs/ratio_25_log.csv
+echo "Running 10% ratio experiment"
+echo "--------------------------------------------------"
+./bin/segregation -c ../config/asymmetric-cell-devs/ratio_10_config.json  -t 500 -o ../logs/asymmetric-cell-devs/ratio_10_log.csv
